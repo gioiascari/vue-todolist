ebfb6fc5c8abdff9c1d782d26ea762d8c7f58e51
Gioia Ascari <gioia.ascari96@gmail.com>
2022-03-28 17:45:32 +0200

    Milestone 1 completato

--- Milestone 1/main.js
+++ Milestone 1/main.js
@@ -1,1 +1,43 @@
 console.log("Ok Vue Js :)");
+/*Descrizione:
+Rifare l'esercizio della to do list.
+Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+- text, una stringa che indica il testo del todo
+- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+MILESTONE 1
+Stampare all'interno di una lista, un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato. */
+const app = new Vue({
+  el: "#todoListVue",
+  data: {
+    todos: [
+      {
+        text: "lorem Ippsum",
+        done: false,
+      },
+      {
+        text: "lorem Ippsum",
+        done: true,
+      },
+      {
+        text: "lorem Ippsum",
+        done: false,
+      },
+      {
+        text: "lorem Ippsum",
+        done: true,
+      },
+    ],
+    textValue: "",
+  },
+  methods: {
+    addTask() {
+      if (this.textValue) {
+        this.todos.push({
+          done: false,
+        });
+      }
+      this.textValue = "";
+    },
+  },
+});
